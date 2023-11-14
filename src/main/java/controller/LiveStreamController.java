4c0dd473ccfccc6ea0a8b17482956b1e7625ef77
jamiecartin <jamie.cartin@icloud.com>
2023-11-14 13:58:03 -0500

    refactoring

--- src/main/java/controller/LiveStreamController.java
+++ src/main/java/controller/LiveStreamController.java
@@ -4,19 +4,24 @@ import model.LiveStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repository.LiveStreamRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/streams")
 public class LiveStreamController {
 
+    private final LiveStreamRepository repository;
+
+    public LiveStreamController(LiveStreamRepository repository) {
+        this.repository = repository;
+    }
 
     // GET http://localhost:8080/streams
     @GetMapping
     public List<LiveStream> findAll() {
-        return null;
+        return repository.findAll();
     }
 
 }
