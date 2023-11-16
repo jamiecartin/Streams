1393e97330f60aae949ef455f3ef6825db2bc2a4
jamiecartin <jamie.cartin@icloud.com>
2023-11-16 16:58:13 -0500

    generate id for findById

--- src/main/java/controller/LiveStreamController.java
+++ src/main/java/controller/LiveStreamController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.LiveStream;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repository.LiveStreamRepository;
@@ -22,6 +23,12 @@ public class LiveStreamController {
     @GetMapping
     public List<LiveStream> findAll() {
         return repository.findAll();
+    }
+
+    // GET http://localhost:8080/streams
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id) {
+        return repository.findById(id);
     }
 
 }
